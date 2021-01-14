6e99fe6150dd890757eeaa85ea87f3239ecdacd4
AnirudhRahul <anirudhrahul@anjurahul.com>
2021-01-13 21:20:55 -0500

    Added google cloud keys

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -5,7 +5,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "708938922173-9p5lthpd7oftf05k6ocmjibea819ra8c.apps.googleusercontent.com";
 
 class Skeleton extends Component {
   constructor(props) {
