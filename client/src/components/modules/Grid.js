9884f7151407996c466f6ff85453cf93a557901a
Emily Jiang <emily.s.jiang@gmail.com>
2021-01-15 18:06:32 -0500

    pollcard stuff idk

--- client/src/components/modules/Grid.js
+++ client/src/components/modules/Grid.js
@@ -9,7 +9,17 @@ class Grid extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            polls: [],
+            polls: [
+                {question: "Am I a Bee?????", // string
+                options: [{_id: 1, content: "yes"}, {_id: 2, content: "no"}, {_id:3, content: "maybe so"}],
+                ownerID: "alicethebee", 
+                voters: [{user: "alicethebee", votes:[1, 2, 3]},
+                        {user: "emilythebee", votes:[1]},
+                        {user: "anithebee", votes:[3]},
+                        {user: "ryanthebee", votes:[1, 3]},
+                        {user: "evethebee", votes:[]},
+                        {user: "ronthebee", votes:[1]}],}
+            ],
         };
     }
 
@@ -18,7 +28,12 @@ class Grid extends Component {
         const hasPolls = this.state.polls.length !== 0;
         if (hasPolls) {
             pollsList = this.state.polls.map((pollObj) => (
-                <PollCard />
+                <PollCard 
+                    question={pollObj.question}
+                    options={pollObj.options}
+                    ownerID={pollObj.ownerID}
+                    voters={pollObj.voters}
+                />
             ));
         } else {
             pollsList = <div>No polls yet! Why not create one?</div>
--- client/src/components/modules/PollCard.js
+++ client/src/components/modules/PollCard.js
@@ -6,24 +6,47 @@ import "../../utilities.css";
 
 /** 
  * Proptypes
- * @param {string} 
+ * @param {string} pollId (fetches info from api using this id?)
 */
 
 class PollCard extends Component {
     constructor(props) {
         super(props);
+        // this.state = { // for now, until we can fetch from backend
+        //     question: "Am I a Bee?????", // string
+        //     options: [{_id: 1, content: "yes"}, {_id: 2, content: "no"}, {_id:3, content: "maybe so"}],
+        //     ownerID: "alicethebee", 
+        //     voters: [{user: "alicethebee", votes:[1, 2, 3]},
+        //               {user: "emilythebee", votes:[1]},
+        //               {user: "anithebee", votes:[3]},
+        //               {user: "ryanthebee", votes:[1, 3]},
+        //               {user: "evethebee", votes:[]},
+        //               {user: "ronthebee", votes:[1]}],
+        //   };
+    }
+
+    componentDidMount() {
+        // fix syntax once backend is good
+        /*get("/api/poll", { pollID: this.props._id }).then((pollObj) => {
+        this.setState({
+            question: pollObj.question,
+            options: pollObj.options,
+            ownerID: pollObj.ownerID,
+            // and MORE COMING SOON!
+        });
+        });*/
     }
 
     render() {
         return (
+            <>
+            <div className="PollCard-namebanner u-textCenter">{this.props.ownerID}</div>
             <div className="PollCard-container">
-                <div className="PollCard-namebanner u-textCenter">{this.props.creator_name}</div>
-                <div>
-                    <p>{this.props.question}</p>
-                    <p className="u-textRight">{this.props.votes.length} votes</p>
-                    <p className="u-textRight">{this.props.votes.length} people</p>
-                </div>
+                <p>{this.props.question}</p>
+                <p className="u-textRight">{this.props.voters.length} votes</p>
+                <p className="u-textRight">{this.props.options.length} options</p>
             </div>
+            </>
         )
     }
 }
