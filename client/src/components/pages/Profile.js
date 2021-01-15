21cab0f27299f006bb62476d6e17eb7c422fcc1c
Emily Jiang <emily.s.jiang@gmail.com>
2021-01-14 21:12:20 -0500

    Added stuff

---
+++ client/src/components/pages/Profile.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Grid from "../modules/Grid.js";
+
+/*
+    props:
+    this.props.user
+*/
+
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        <div className="Profile-container">
+
+        </div>
+    }
+}
