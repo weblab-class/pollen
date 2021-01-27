be13b97081f221819de43f773e3f7e658d569d36
Alice Chen <chenalice2020@gmail.com>
2021-01-27 04:07:55 -0600

    sutf

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -68,56 +68,64 @@ class Profile extends Component {
         return post('/api/poll', {question: question, tags: tags});
     }
 
-    render() {
-//        //console.log(this.state.user);
-        if (!this.state.show_create)
-        {
-            //console.log("PICTURE", this.state.user.picture_link)
-            const pfpborder = {
-                border: this.state.user.border_color + " 2px dashed",
-            };
-            return (
-                <div className="App-container">
-                <div className="Profile-container">
-                    <div className="Profile-sidebar">
-                        <img className="Profile-pfp"
-                                src={this.state.user.picture_link} alt="bee"
-                                style={pfpborder}
-                                width="120px" height="120px" />
-                        <h2 className="Profile-displayname">{this.state.user.displayName}</h2>
-                        <p className="Profile-username">{this.state.user.userTag}</p>
-                        <div className="Profile-sidebuttons u-flexColumn">
-                            <div className="Profile-subTop">
-                                <div className="Profile-option u-morepadding u-pointer" onClick={this.viewMyPolls} >my polls</div>
-                                <div className="Profile-option u-morepadding u-pointer" onClick={this.viewShared} >shared with me</div>
-                            </div>
-                            <div className="Profile-subBot">
-                                <button
-                                    type="submit"
-                                    value="Create Poll"
-                                    onClick={this.createPoll}
-                                    className="Profile-button u-pointer"> + create a poll </button>
-                                <button
-                                    type="submit"
-                                    value="Edit Profile"
-                                    onClick={() => {window.location.href = "/profile/edit";}}
-                                    className="Profile-button u-pointer"> &nbsp;&nbsp;&nbsp;edit profile&nbsp;&nbsp;&nbsp; </button>
-                            </div>
-                        </div>
-                    </div>
+    render() 
+    {
 
-                    <div>
-                        <h2 className="Profile-view">  {this.state.view}</h2>
-                        <Grid tagColors={this.props.tagColors} tagIcons={this.props.tagIcons} user={this.state.user} view={this.state.view} />
-                    </div>
-                </div>
-                </div>
-                );
+        if (Object.keys(this.state.user).length === 0)
+        {
+            return <></>;
         }
         else
         {
-            return <Modal tagColors={this.props.tagColors} addNewPoll={this.addNewPoll} show={this.state.show_create} closeCreator={this.closeCreator}/>;
-        }
+            if (!this.state.show_create)
+            {
+                //console.log("PICTURE", this.state.user.picture_link)
+                const pfpborder = {
+                    border: this.state.user.border_color + " 2px dashed",
+                };
+                return (
+                    <div className="App-container">
+                    <div className="Profile-container">
+                        <div className="Profile-sidebar">
+                            <img className="Profile-pfp"
+                                    src={this.state.user.picture_link} alt="bee"
+                                    style={pfpborder}
+                                    width="120px" height="120px" />
+                            <h2 className="Profile-displayname">{this.state.user.displayName}</h2>
+                            <p className="Profile-username">{this.state.user.userTag}</p>
+                            <div className="Profile-sidebuttons u-flexColumn">
+                                <div className="Profile-subTop">
+                                    <div className="Profile-option u-morepadding u-pointer" onClick={this.viewMyPolls} >my polls</div>
+                                    <div className="Profile-option u-morepadding u-pointer" onClick={this.viewShared} >shared with me</div>
+                                </div>
+                                <div className="Profile-subBot">
+                                    <button
+                                        type="submit"
+                                        value="Create Poll"
+                                        onClick={this.createPoll}
+                                        className="Profile-button u-pointer"> + create a poll </button>
+                                    <button
+                                        type="submit"
+                                        value="Edit Profile"
+                                        onClick={() => {window.location.href = "/profile/edit";}}
+                                        className="Profile-button u-pointer"> &nbsp;&nbsp;&nbsp;edit profile&nbsp;&nbsp;&nbsp; </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <h2 className="Profile-view">  {this.state.view}</h2>
+                            <Grid tagColors={this.props.tagColors} tagIcons={this.props.tagIcons} user={this.state.user} view={this.state.view} />
+                        </div>
+                    </div>
+                    </div>
+                    );
+            }
+            else
+            {
+                return <Modal tagColors={this.props.tagColors} addNewPoll={this.addNewPoll} show={this.state.show_create} closeCreator={this.closeCreator}/>;
+            }
+        }   
     }
 }
 
