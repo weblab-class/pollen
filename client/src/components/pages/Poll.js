49abfad138c0c730ecc8e38792638fa91f8ebebc
Emily Jiang <emily.s.jiang@gmail.com>
2021-01-27 03:20:28 -0500

    Profile formatting

--- client/src/components/pages/Poll.js
+++ client/src/components/pages/Poll.js
@@ -300,7 +300,7 @@ class Poll extends Component
       }
 
         return (
-          <div >
+          <div style={{maxWidth: "1200px", margin: "auto"}}>
             <div className="Poll-container">
 
             <div className="Poll-head">
