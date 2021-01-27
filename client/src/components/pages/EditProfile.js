18f1b004de9506fcd9815f89be9f552508800b29
Emily Jiang <emily.s.jiang@gmail.com>
2021-01-26 22:58:58 -0500

    EditProfile works!!!!!!

--- client/src/components/pages/EditProfile.js
+++ client/src/components/pages/EditProfile.js
@@ -4,6 +4,7 @@ import { get, post } from "../../utilities.js";
 import "../../utilities.css";
 import "./Profile.css";
 import "../modules/Modal.css";
+import "../modules/NewOption.css";
 
 /*
     props:
@@ -16,6 +17,8 @@ class EditProfile extends Component {
     super(props);
     this.state = {
       user: {},
+      usernameBox: "",
+      pfp_loc: "/images/pfp/defaultpfp.svg",
     }
   }
 
@@ -28,28 +31,26 @@ class EditProfile extends Component {
     });
   }
 
-  // example
-  /* handleAddVote = async opt_id =>
-  {
-    const body = {id: this.state.poll._id, option_id: opt_id};
-    const pollObj = await post("/api/poll/vote", body);
+  handleChangePfp = (pfp_loc) => {
+    return async() => {
+      const body = {picture_link: pfp_loc};
+      const userObj = await post("/api/user/self", body);
+      this.setState({
+        user: userObj,
+      });
+    }
+  }
 
+  handleTypingUsername = (e) => {
     this.setState({
-      poll: pollObj,
-    });
-  } */
-
-  // api endpoint to change user icon
-  handleChangePfp = async pfp_loc => {
-    const userObj = await post("/api/edit/icon", pfp_loc);
-    this.setState({
-      user: userObj,
+      usernameBox: e.target.value,
     });
   }
 
   // api endpoint to change username
-  handleChangeUsername = async username => {
-    const userObj = await post("/api/edit/username", username);
+  handleChangeUsername = async() => {
+    const body = {userTag: "@" + this.state.usernameBox};
+    const userObj = await post("/api/user/self", body);
     this.setState({
       user: userObj,
     });
@@ -59,38 +60,45 @@ class EditProfile extends Component {
     let pfpList = null;
     if (this.props.beeIcons.length !== 0) {
       pfpList = this.props.beeIcons.map((icon) => (
-        <img className="Profile-pfp" src={icon} alt="bee" width="100px" height="100px" />
+        <img src={icon} alt="bee" width="100px" height="100px"
+          className="EditProfile-pfp u-pointer" 
+          onClick={this.handleChangePfp(icon)}
+        />
       ));
     } else {
       pfpList = <div>no icons available :(</div>;
     }
     return (
       <div> 
-      <h1 className="u-textCenter">edit profile</h1> 
-      {/* so basically show a row of circle pfps and when they click to choose make the circle bigger */}
-      <h2 className="EditProfile-heading" ><span className="u-textCenter EditProfile-highlightPurple" > &nbsp;&nbsp;pick a new profile icon!&nbsp;&nbsp; </span></h2>
-      <div className="EditProfile-pfpContainer">
-        {pfpList}
-      </div>
-      <h2 className="EditProfile-heading" ><span className="u-textCenter EditProfile-highlightPurple" > &nbsp;&nbsp;change your username:&nbsp;&nbsp; </span></h2>
-      <div className="u-flex Modal-box Modal-topbox">
-          <input
-              type="text"
-              placeholder="bobthebee"
-              // value={this.state.question}
-              // onChange={this.handleQuestionChange}
-              className="Modal-input"
-          />
-      </div>
-      <div className = "u-flex Modal-box">
-        <button
-            type="submit"
-            value="Change Username"
-            // onClick={this.handleSubmit}
-            className="Modal-submit u-pointer"
-        > change </button>
-        {/* <button className="Modal-close" onClick={this.props.closeCreator}>Close</button> */}
-      </div>
+        <br/>
+        <h1 className="u-textCenter">edit profile</h1> 
+        {/* so basically show a row of circle pfps and when they click to choose make the circle bigger */}
+        <br/>
+        <h2 className="EditProfile-heading" ><span className="u-textCenter EditProfile-highlightPurple" > &nbsp;&nbsp;pick a new profile icon!&nbsp;&nbsp; </span></h2>
+        <div className="EditProfile-pfpContainer">
+          {pfpList}
+        </div>
+        <br/>
+        <h2 className="EditProfile-heading" ><span className="u-textCenter EditProfile-highlightPurple" > &nbsp;&nbsp;change your username:&nbsp;&nbsp; </span></h2>
+
+        <div className="u-flex" style={{justifyContent: "center"}}>
+            <input
+                type="text"
+                placeholder="bobthebee"
+                value={this.state.usernameBox}
+                onChange={this.handleTypingUsername}
+                className="Modal-input"
+                style={{marginRight: "1px"}}
+            />
+          <button
+              type="submit"
+              value={this.state.usernameBox}
+              onClick={this.handleChangeUsername}
+              className="Modal-submit u-pointer"
+              style={{marginLeft: "1px"}}
+          > change </button>
+          {/* <button className="Modal-close" onClick={this.props.closeCreator}>Close</button> */}
+        </div>
       </div>
     );
   }
